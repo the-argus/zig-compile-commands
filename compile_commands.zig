c8b36397a0bd8a521165a41d7a9c1310ada402c6
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-08-25 16:08:56 -0400

    handle error

--- compile_commands.zig
+++ compile_commands.zig
@@ -45,7 +45,7 @@ fn extractIncludeDirsFromCompileStepInner(b: *std.Build, step: *std.Build.Step.C
                 var local_lazy_path_output = std.ArrayList(std.Build.LazyPath).init(b.allocator);
                 defer local_lazy_path_output.deinit();
                 extractIncludeDirsFromCompileStepInner(b, other_step, &local_lazy_path_output);
-                lazy_path_output.appendSlice(local_lazy_path_output.items);
+                lazy_path_output.appendSlice(local_lazy_path_output.items) catch @panic("OOM");
             },
             .path => |path| lazy_path_output.append(path) catch @panic("OOM"),
             .path_system => |path| lazy_path_output.append(path) catch @panic("OOM"),
