50b061b72496ed7cacdac8c5444c6884315711e3
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-09-05 15:33:11 -0400

    update flake to use zig 13 in environment

--- flake.nix
+++ flake.nix
@@ -1,20 +1,32 @@
 {
-  description = "compile_commands generation for the zig build system. flake only supports x86_64-linux.";
+  description = "compile_commands generation for the zig build system";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShell.${system} =
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    supportedSystems = let
+      inherit (flake-utils.lib) system;
+    in [
+      system.aarch64-linux
+      system.x86_64-linux
+    ];
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell =
         pkgs.mkShell
-          {
-            packages = with pkgs; [
-              zig_0_11
-            ];
-          };
-    };
+        {
+          packages = with pkgs; [
+            zig_0_13
+          ];
+        };
+    });
 }
